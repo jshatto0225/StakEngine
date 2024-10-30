3943cdd6f209d861c7e848d260d7571de542399d
jshatto0225 <jshatto0225@vt.edu>
2024-10-30 14:41:10 -0400

    forgot to add files

---
+++ StakEditor/Editor.h
@@ -0,0 +1,6 @@
+#include "../StakEngine/StakEngine.h"
+
+class Editor : public Stak::Application {
+public:
+  Editor(Stak::ApplicationSpec &spec);
+};
---
+++ StakEngine/premake5.lua
@@ -0,0 +1,25 @@
+project "StakEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
