4a3143f134cf3eea033068f313e3e622b5cbd144
jshatto0225 <jshatto0225@gmail.com>
2025-03-10 04:37:57 -0400

    Tested externalincludedirs, needs to be tested on mac and linux, but it works on windows

--- StakEngine/premake5.lua
+++ StakEngine/premake5.lua
@@ -1,60 +1,68 @@
 project "StakEngine"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++20"
-	staticruntime "off"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
 
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files {
-		"Source/**.h",
-		"Source/**.cpp",
-		"StakEngine.h"
-	}
+    files {
+        "Source/**.h",
+        "Source/**.cpp",
+        "StakEngine.h"
+    }
 
-	includedirs {
-		"Source",
-		"Source/Core",
-		"Source/Platform/GLFW",
-		"Source/Platform/Vulkan",
-		"Source/Renderer",
-		"Source/RHI",
-		"Source/ImGui",
-		"External/imgui",
-		"External/spdlog/include",
-	}
+    includedirs {
+        "Source",
+        "Source/Core",
+        "Source/Platform/GLFW",
+        "Source/Platform/Vulkan",
+        "Source/Renderer",
+        "Source/RHI",
+        "Source/ImGui",
+    }
 
-	links {
-		"ImGui"
-	}
+    externalincludedirs {
+        "External/imgui",
+        "External/spdlog/include",
+        "External/glfw/include",
+    }
 
-	filter "system:windows"
-		systemversion "latest"
-		defines {
-			"SK_WINDOWS",
-			"SK_VULKAN",
-			"SK_GLFW"
-		}
-		links { 
-			"$(VULKAN_SDK)/lib/vulkan-1.lib",
-			"GLFW"
-		}
-		includedirs {
-			"External/glfw/include",
-			"$(VULKAN_SDK)/include"
-		}
+    defines {    
+        "SK_GLFW"
+    }
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		defines {
-			"SK_DEBUG"
-		}
-		symbols "on"
+    links {
+        "ImGui",
+        "GLFW",
+    }
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "system:windows"
+        systemversion "latest"
+        defines {
+            "SK_WINDOWS",
+            "SK_VULKAN",
+        }
+        
+        links { 
+            "$(VULKAN_SDK)/lib/vulkan-1.lib",
+        }
+        
+        includedirs {
+            "$(VULKAN_SDK)/include"
+        }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        defines {
+            "SK_DEBUG"
+        }
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
 
 include "External/glfw.lua"
 include "External/imgui.lua"
